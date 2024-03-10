605df3a6b255da85941b6b0102685d7b2ff535fa
oreo <88177661+superoreoooooo@users.noreply.github.com>
2024-03-10 11:09:35 +0900

    Update D0307.c

--- D0307.c
+++ D0307.c
@@ -7,5 +7,11 @@ int main() {
     printf("%c\n", "abc");
     printf("%d\n", 123);
     printf("%f\n", 123.456);
+    char str[3];
+    str[0] = "a";
+    str[1] = "b";
+    str[2] = "c"; 
+
+
     return 0;
 }
