cf8e42e85e7e0b0284341d8e5aaa653fe5732bf9
oreo <88177661+superoreoooooo@users.noreply.github.com>
2024-03-28 11:13:36 +0900

    Update practice.c

--- practice.c
+++ practice.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void getTotal(double *total, double hour, int pay) {
+    *total = (hour * pay);
+}
 
 int main() {
-    printf("Hello World!");
+    int pay = 10000;
+    double hour = 42.5;
+    double total = 0;
+    getTotal(&total, hour, pay);
+    
+    printf("시급 %d, %.1f시간 일함\n총합 = %.0f", pay, hour, total);
     return 0;
 }
